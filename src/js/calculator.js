8fc11be0026359e0a4178a0bb1439b56ba677183
i-webtest <stasbass80@gmail.com>
2023-05-31 15:14:14 +0300

    custom select

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -13,3 +13,22 @@
 
 // btn.addEventListener('click', closeSelect);
 // btn2.addEventListener('click', closeSelect2);
+const btn = document.querySelector('.itc-select2__toggle');
+console.log('btn: ', btn.textContent);
+const option = document.querySelectorAll('.itc-select2__option');
+
+option.forEach((item) => {
+  item.addEventListener('change', () => {
+    btn.textContent = item.textContent;
+  });
+});
+
+const btn6 = document.querySelector('.itc-select6__toggle');
+console.log('btn: ', btn.textContent);
+const option6 = document.querySelectorAll('.itc-select6__option');
+
+option6.forEach((item) => {
+  item.addEventListener('change', () => {
+    btn6.textContent = item.textContent;
+  });
+});
